7003c622a9fff5a73b8f2d1d2b2ceb4064bb5650
Claudia Hernández Bechiarelli <Claudia Hernndez Bechiarelli>
2025-10-10 15:22:29 +0200

    lastw

--- Rank2/Level1/last_word2.c
+++ Rank2/Level1/last_word2.c
@@ -12,10 +12,10 @@ int	main(int ac, char **av)
 		i--;
 		while(av[1][i] == ' ' || av[1][i] == '\t')
 			i--;
-		while(av[1][i] > 32)
+		while(av[1][i] > ' ')
 			i--;
 		i++;
-		while(av[1][i] && av[1][i] > 32)
+		while(av[1][i] && av[1][i] > ' ')
 		{
 			write(1, &av[1][i], 1);
 			i++;
