17b23b0387e896a5d7a920c40cfa6871bbc33345
Claudia Hernández Bechiarelli <claherna@c2r2s3.42malaga.com>
2025-10-16 17:55:39 +0200

    revwstr good

--- Rank2/Level1/reverse_bits.c
+++ Rank2/Level1/reverse_bits.c
@@ -15,11 +15,21 @@ unsigned char	reverse_bits(unsigned char octet)
 
 /* reverse_bit (invierte los bits de un byte)
  * Recibe un número tipo unsigned char (8 bits) y devuelve otro con los bits en orden inverso.
- * Creamos:
- * 	- res = 0 -> para almacenar el nuevo byte.
- * 	- i = 8 -> porque un byte tiene 8 bits.
+ * Creamos dos variables:
+ * 	-unsigned char res = 0 -> para almacenar el nuevo byte.
+ * 	-int i = 8 -> porque un byte tiene 8 bits.
  * Mientras i decrece:
- * 	- Tomamos el último bit de octet con (octet & 1)
+ * 	- el res es = a (res << 1) 
+	- Añadimos ese bit con el operador OR (|) que une los bits uno a uno.
+	- Tomamos el último bit de octet con (octet & 1)
+Como realmente lo leemos de derecha a izquierda, primero tomamos el último bit de octet,
+luego desplazamos res una posición a la izquierda y entre medias añadimos el operador OR.
+	- Desplazamos octet una posición a la derecha con octet >>= 1 (octet = (octet >> 1)
+	así leemos el siguiente bit.
+Al terminar, devolvemos res, que ahora contiene los bits al revés. 
+
+COMO ESTABA ANTES: 
+Tomamos el último bit de octet con (octet & 1)
  * 	- Desplazamos res una posición a la izquierda (res << 1)
  * 	- Añadimos ese bit con el operador OR (|) que une los bits uno a uno.
  * 	- Desplazamos octet una posición a la derecha (octet >> 1) para leer el siguiente bit.
--- Rank2/Level3/rev_wstr.c
+++ Rank2/Level3/rev_wstr.c
@@ -5,12 +5,14 @@ int	main(int ac, char **av)
 	int	i;
 	int	start;
 	int	end;
+	int	word = 0;
 
 	if(ac == 2)
 	{
 		i = 0;
 		while(av[1][i])
 			i++;
+		i--;
 		while(i >= 0)
 		{
 			while(i >= 0 && (av[1][i] == ' ' || av[1][i] == '\t'))
@@ -21,11 +23,11 @@ int	main(int ac, char **av)
 			start = i + 1;
 			if(start <= end)
 			{
-				write(1, &av[1][start], end - start + 1);
-				if(i > 0)
+				if(word)
 					write(1, " ", 1);
+				write(1, &av[1][start], end - start + 1);
+				word = 1;
 			}
-			
 		}
 	}
 	write(1, "\n", 1);
@@ -34,17 +36,22 @@ int	main(int ac, char **av)
 
 /* rev_wstr (imprime palabras en orden inverso)
  * Recibe una cadena como argumento y muestra las palabras, desde la última hasta la primera.
- * Creamos tres variables de tipo integrer:
+ * Creamos cuatro variables de tipo integrer:
  * 	- i -> recorre la cadena.
  * 	- start -> marca el inicio de cada palabra
  * 	- end -> marca el final de cada palabra.
+	- word -> palabro nueva, es una bandera, la igualamos a 0 hasta que tengamos una palabro.
  * 1. Comprobamos que hay un solo argumento.
- * 2. Recorremos el argumento porque existe.
- * 3. Mientras que i >= 0 recorremos hacia atrás. ¿Cómo?
+ * 2. Recorremos el argumento porque existe. i++ hasta el final.
+ 2.1 i--; para ir a antes del nulo. 
+ * 3. Mientras que i >= 0 recorremos hacia atrás.
  * 	- (Mientras) Saltamos espacios y tabs. Iteramos patrás.
  * 	- Guardamos en end la última letra de la palabra.
  * 	- (Mientras) Retrocedemos hasta el inicio de la palabra. Iteramos patrás.
  * 	- Guardamos en start la posición de i + 1, para marcar la primera letra de la palabra.
- * 	- SI start es menor o igual a end, escribimos &av[1][start] con el size de end - start + 1.
- * 	- Si i > 0, escribimos un espacio.
+ * 	- SI start es menor o igual a end
+		- Si word existe.
+			- Escribimos un espacio.
+		- Escribimos &av[1][start] con el size de end - start + 1.
+ * 	- la flag word = 1;
  * 	- Salimos del bucle, salto de línea, return 0. */
