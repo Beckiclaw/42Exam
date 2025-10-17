7d0ebc2eee1594e95937d25ceb42dac3b1a959ef
Claudia Hernández Bechiarelli <Claudia Hernndez Bechiarelli>
2025-10-17 14:35:48 +0200

    sorttab

--- Rank2/Level3/sort_int_tab.c
+++ Rank2/Level3/sort_int_tab.c
@@ -15,23 +15,18 @@ void	ft_swap(int *a, int *b)
 void	sort_int_tab(int *tab, unsigned int size)
 {
 	unsigned int	i;
-	int	swapped;
 
 	if(size < 2)
 		return;
-	swapped = 1;
-	while(swapped)
+	i = 0;
+	while(i < size - 1)
 	{
-		swapped = 0;
-		i = 0;
-		while(i + 1 < size)
+		if(tab[i] > tab[i + 1])
 		{
-			if(tab[i] > tab[i + 1]);
-			{
-				ft_swap(&tab[i], &tab[i + 1]);
-				swapped = 1;
-			}
-			i++;
+			ft_swap(&tab[i], &tab[i + 1]);
+			i = 0;
 		}
+		else
+			i++;
 	}
 }
