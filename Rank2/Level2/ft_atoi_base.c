05333a0ecc8cb2d0bfefaef582e8a1906e3124e4
Claudia Hernández Bechiarelli <claherna@c3r9s6.42malaga.com>
2025-10-30 18:15:06 +0100

    atoi base renew

--- Rank2/Level2/ft_atoi_base.c
+++ Rank2/Level2/ft_atoi_base.c
@@ -1,4 +1,6 @@
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int	ft_atoi_base(const char *str, int str_base)
 {
@@ -31,5 +33,56 @@ int	ft_atoi_base(const char *str, int str_base)
 		i++;
 	}
 	return (res * sign);
+}
+
+/*
+ALTERNATIVAS para calcular value:
+
+1. CON TERNARIO (una línea):
+	value = (str[i] >= '0' && str[i] <= '9') ? str[i] - '0' :
+	        (str[i] >= 'a' && str[i] <= 'f') ? str[i] - 'a' + 10 :
+	        (str[i] >= 'A' && str[i] <= 'F') ? str[i] - 'A' + 10 : -1;
+
+2. CON SWITCH (si tienes disponible):
+	switch(str[i])
+	{
+		case '0': case '1': ... case '9':
+			value = str[i] - '0';
+			break;
+		case 'a': case 'b': ... case 'f':
+			value = str[i] - 'a' + 10;
+			break;
+		...
+	}
+
+3. CON FUNCIÓN AUXILIAR (más limpio):
+	int get_value(char c)
+	{
+		if(c >= '0' && c <= '9')
+			return (c - '0');
+		if(c >= 'a' && c <= 'f')
+			return (c - 'a' + 10);
+		if(c >= 'A' && c <= 'F')
+			return (c - 'A' + 10);
+		return (-1);
+	}
+
+4. CON BÚSQUEDA EN STRING (alternativa creativa):
+	const char *digits = "0123456789abcdefABCDEF";
+	char *pos = ft_strchr(digits, str[i]);
+	if (pos)
+	{
+		value = (pos - digits);
+		if (value > 15)
+			value -= 6;  // Ajustar mayúsculas
+	}
+
+La más común es TERNARIO (opción 1) o la función auxiliar (opción 3).
+*/
+
+int	main(void)
+{
+	printf("%d\n", ft_atoi_base("--223", 16));
+	return (0);
 }
 
